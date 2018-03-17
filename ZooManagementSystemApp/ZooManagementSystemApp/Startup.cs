799d10746e45e15ef92ad68582d819142e52db95
NoraAlamJiku <nora.alam07@gmail.com>
2018-03-17 17:07:32 +0600

    first commit

---
+++ ZooManagementSystemApp/ZooManagementSystemApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ZooManagementSystemApp.Startup))]
+namespace ZooManagementSystemApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
